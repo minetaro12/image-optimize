90783f0c1d158215d4ae93b1fd2908cdd6312699
minetaro12 <osawa2003@gmail.com>
2022-02-06 14:06:41 +0900

    画質 フォーマットを指定できるように

--- index.js
+++ index.js
@@ -3,7 +3,13 @@ const express = require('express');
 const app = express();
 
 const multer = require('multer');
-const upload = multer({ dest: 'tmp/' });
+const upload = multer({
+  dest: 'tmp/',
+  limits: {
+    fileSize: 10485760, //10MB
+    files: 1
+  }
+});
 
 const sharp = require('sharp');
 const sizeOf = require('image-size');
@@ -17,22 +23,41 @@ app.get('/upload', (req, res) => {
 
 app.post('/upload', upload.single('file'), (req, res) => {
   try {
-    console.log(req.file.path);
-    var outpath = (req.file.path + '_out.jpg'); //処理後のファイル名
+    console.log('\n' + req.file.path + '\n quality:' + req.body.quality + '\n format:' + req.body.format + '\n size:' + req.body.size);
     var image = sharp(req.file.path);
-    if (req.body.size) { //sizeが指定されている場合はリサイズ
-      var dimensions = sizeOf(req.file.path);
+    if (!isNaN(req.body.size) && req.body.size != '' && req.body.size != '100') { //sizeが指定されている場合はリサイズ
+      var dimensions = sizeOf(req.file.path); //元画像のサイズを取得
       var outwidth = Math.round(dimensions.width * ( req.body.size / 100 )); //sizeからサイズを計算&四捨五入
       image.resize(outwidth, null);
+      console.log('Resized');
     };
-    image.toFormat('jpg', {
-      quality: 75
-    });
+    if (!isNaN(req.body.quality) && req.body.quality != '') { //qualityがあれば指定
+      var numquality = Number(req.body.quality);
+      if (1 <= numquality <= 100) {
+        var outquality = numquality;
+      } else {
+        var outquality =75;
+      };
+    } else { //なければ75
+      var outquality = 75;
+    };
+    if (req.body.format == 'jpeg' || req.body.format == 'jpg' || req.body.format == 'png' || req.body.format == 'webp') { //formatがあれば指定
+      image.toFormat(req.body.format, {
+        quality: outquality
+      });
+      var outname = req.file.filename + '_out.' + req.body.format;
+    } else {
+      image.toFormat('jpg', { //なければjpg
+        quality: outquality
+      });
+      var outname = req.file.filename + '_out.jpg';
+    };
+    var outpath = 'tmp/' + outname
     image.toFile(outpath, (err, info) => {
       console.log(info);
+      console.log(outpath);
       var outfile = fs.readFileSync(outpath);
-      var dlfilename = req.file.filename + '_out.jpg'
-      res.set({'Content-Disposition': `attachment; filename=${dlfilename}`});
+      res.set({'Content-Disposition': `attachment; filename=${outname}`});
       res.send(outfile);
       fs.unlinkSync(req.file.path);
       fs.unlinkSync(outpath);
@@ -40,6 +65,14 @@ app.post('/upload', upload.single('file'), (req, res) => {
   } catch(e) {
     res.status(500).send('Error');
     console.log(e);
+    fs.unlinkSync(req.file.path, (err) => {
+      console.log('do not exist');
+    });
+    if(outpath) {
+      fs.unlinkSync(outpath, (err) => {
+       console.log('do not exist');
+      });
+    };
   };
 });
 
