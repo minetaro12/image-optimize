3508d59a8bf61537a19ee630c73cdb05247e240b
minetaro12 <osawa2003@gmail.com>
2022-02-24 12:22:07 +0000

    クライアントの追加

--- src/index.ts
+++ src/index.ts
@@ -5,8 +5,10 @@ const port = process.env.PORT || 8000;
 app.use(express.urlencoded({extended: true}));
 
 import uploadProcess from './upload';
-
 app.use('/upload', uploadProcess);
+
+import htmlClient from './client';
+app.use('/', htmlClient);
 
 app.listen(port, () => {
   console.log(`Server listen on port ${port}`);
