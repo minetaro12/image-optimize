01899d4bc83a7cd237987603120d6569b772869b
minetaro12 <osawa2003@gmail.com>
2022-03-03 18:52:00 +0900

    Add bootstrap

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ const app = express();
 const port = process.env.PORT || 8000;
 
 app.use(express.urlencoded({extended: true}));
+app.use(express.static('public'));
 
 import uploadProcess from './upload';
 app.use('/upload', uploadProcess);
