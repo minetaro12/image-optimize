4bb107994ae30e0310f08d9f79c7f2df05b1661a
minetaro12 <osawa2003@gmail.com>
2022-05-14 18:13:33 +0900

    bufferを使用するように変更

--- src/upload.ts
+++ src/upload.ts
@@ -2,23 +2,21 @@ import express from 'express';
 const router = express.Router();
 
 import multer from 'multer';
+//const inBuffer = multer.memoryStorage();
 const upload = multer({
-  dest: 'tmp/',
   limits: {
     fileSize: 10485760, //10MB
     files: 1
   }
 });
 
-import fs from 'fs';
 import sharp from 'sharp';
 import sizeOf from 'image-size';
-import remove from './remove';
+import path from 'path';
 
 router.post('/', upload.single('file'), (req,res) => {
   if (req.file) {
-    const originPath: string = req.file.path;
-    const originName: string = req.file.filename;
+    const originName: string = req.file.originalname;
     const originMime: string = req.file.mimetype;
 
     const sizeOp: string = req.body.size;
@@ -27,17 +25,19 @@ router.post('/', upload.single('file'), (req,res) => {
     const removemetaOp: string = req.body.removemeta;
     const grayOp: string = req.body.gray;
 
-    let outQuality: number;
-    let outPath: string|undefined;
-    let outName: string;
-
      (async () => {
       try {
         //画像かどうか
         if (originMime != 'image/jpeg' && originMime != 'image/png' && originMime != 'image/webp') {throw new Error('Invalid type')};
-        console.log(`\n${originPath}\nmimetype: ${originMime}\nresize: ${sizeOp}\nquality: ${qualityOp}\nformat: ${formatOp}\nremovemeta: ${removemetaOp}\ngrayscale: ${grayOp}\n`);
+        console.log(`\n${originMime}\nresize: ${sizeOp}\nquality: ${qualityOp}\nformat: ${formatOp}\nremovemeta: ${removemetaOp}\ngrayscale: ${grayOp}\n`);
 
-        const image = sharp(originPath);
+        let inBuffer: Buffer;
+        if (req.file?.buffer) {
+          inBuffer = req.file.buffer;
+        } else {
+          throw new Error('Buffer not fount');
+        };
+        const image = sharp(inBuffer);
 
         if (removemetaOp == '1') { //メタデータ削除が指定された場合
           console.log('Remove meta');
@@ -47,7 +47,7 @@ router.post('/', upload.single('file'), (req,res) => {
         };
 
         if (sizeOp && sizeOp != '' && sizeOp != '100') { //sizeが指定されている場合はリサイズ
-          const inWidth: number|undefined = sizeOf(originPath).width; //元画像の横サイズを取得
+          const inWidth: number|undefined = sizeOf(inBuffer).width; //元画像の横サイズを取得
           if (inWidth) {
             const numSize: number = Number(sizeOp);
             const outWidth: number = Math.round(inWidth * ( numSize / 100 )); //sizeからサイズを計算&四捨五入
@@ -56,6 +56,7 @@ router.post('/', upload.single('file'), (req,res) => {
           };
         };
 
+        let outQuality: number;
         if (qualityOp && qualityOp != '') { //qualityがあれば指定
           const numQuality: number = Number(qualityOp);
           if (1 <= numQuality && numQuality <= 100) {
@@ -72,42 +73,35 @@ router.post('/', upload.single('file'), (req,res) => {
           console.log('Grayscale');
         };
 
+        let outBuffer: Buffer;
+        let outName: string;
+        const filenameParsed = path.parse(originName).name;
         if (formatOp == 'png' || formatOp == 'webp') { //formatがあれば指定
-          outName = `${originName}_out.${formatOp}`;
-          outPath = `${originPath}_out.${formatOp}`;
-          await image
+          outName = `${filenameParsed}_out.${formatOp}`;
+          outBuffer = await image
             .toFormat(formatOp, {
               quality: outQuality
             })
-            .toFile(outPath);
-          console.log(outPath);
+            .toBuffer();
         } else {
-          outName = `${originName}_out.jpg`;
-          outPath = `${originPath}_out.jpg`;
-          await image
+          outName = `${filenameParsed}_out.jpg`;
+          outBuffer = await image
             .toFormat('jpg', {
               mozjpeg: true,
               quality: outQuality
             })
-            .toFile(outPath);
-          console.log(outPath);
+            .toBuffer()
         };
 
         //ファイルの送信
-        const outFile: Buffer = fs.readFileSync(outPath);
         res.set({'Content-Disposition': `attachment; filename=${outName}`});
-        res.send(outFile);
-
-        //一時ファイルの削除
-        remove(originPath);
-        remove(outPath);
+        res.send(outBuffer);
 
       } catch(e) {
         console.log(e);
         res.status(500).send('Error');
-        remove(originPath)
-        if (outPath) {remove(outPath)};
       }
+      
     })();
   } else {
     res.status(500).send('Error');
